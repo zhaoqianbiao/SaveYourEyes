9c6ad2800ce4f0cd08a52b8f2cfa14b53f16937e
QianbiaoZhao <69677477+zhaoqianbiao@users.noreply.github.com>
2026-03-24 23:09:02 -0500

    Create stub codes

--- SaveYourEyes/ContentView.swift
+++ SaveYourEyes/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What should you do to protect your treasured eyes?")
         }
         .padding()
     }
