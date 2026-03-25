8f2acc9b679e90813a34485726e89d72b7be512d
QianbiaoZhao <69677477+zhaoqianbiao@users.noreply.github.com>
2026-03-24 22:55:14 -0500

    Initial Commit

---
+++ SaveYourEyes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SaveYourEyes
+//
+//  Created by Quincy Z on 3/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SaveYourEyes/SaveYourEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveYourEyesApp.swift
+//  SaveYourEyes
+//
+//  Created by Quincy Z on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct SaveYourEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
